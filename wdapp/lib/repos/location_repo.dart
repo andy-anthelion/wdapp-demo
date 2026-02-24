49fba242882ebe6e1ec57669531e3f7c1400293b
Andy A <andy.stow.away@gmail.com>
2026-02-24 13:35:45 +0530

    location service and repo update

--- wdapp/lib/repos/location_repo.dart
+++ wdapp/lib/repos/location_repo.dart
@@ -9,19 +9,31 @@ class LocationRepo {
   }):_locationService = locationService;
 
   Map<String, String>? _cachedLocations;
+  Map<String, List<String>>? _cachedIndex;
 
   final LocationService _locationService;
 
   Future<Result<String>> getLocationName(String locCode) async {
-    
     try {
       _cachedLocations ??= (await _locationService.getLocations()).getOrThrow();
       String? name = _cachedLocations![locCode];
-      return name != null ? Success(name) : Failure(Exception("not found"));
+      return name != null ? Success(name) : Failure(Exception("name not found"));
     } on Exception catch(e) {
       return Failure(e);
     }
   }
 
+  Future<Result<List<String>>> getCodesForPrefix(String prefix) async {
+    try {
+      _cachedIndex ??= (await _locationService.getIndex()).getOrThrow();
+      if(_cachedIndex?.containsKey(prefix) == false) {
+        return Success([]);
+      }
+      List<String>? codes = _cachedIndex![prefix];
+      return codes != null ? Success(codes) : Failure(Exception("codes not present"));
+    } on Exception catch(e) {
+      return Failure(e);
+    }
+  }
   
 }
--- wdapp/lib/services/location_service.dart
+++ wdapp/lib/services/location_service.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'dart:io';
+import 'package:flutter/services.dart';
 
 import 'package:result_dart/result_dart.dart';
 
@@ -9,7 +9,7 @@ class LocationService {
 
   Future<Result<Map<String, String>>> getLocations() async {
     try {
-      var fileContent = await File(Assets.locations).readAsString();
+      var fileContent = await rootBundle.loadString(Assets.locations);
       var locations = jsonDecode(fileContent).cast<String,String>();
       return Success(locations);
     } on Exception catch(e) {
@@ -17,4 +17,19 @@ class LocationService {
     }
   }
 
+  Future<Result<Map<String, List<String>>>> getIndex() async {
+    try {
+      Map<String, List<String>> index = {};
+      var fileContent = await rootBundle.loadString(Assets.locationIndex);
+      var content = jsonDecode(fileContent).cast<String, dynamic>();
+      for(final entry in content.entries) {
+        String prefix = entry.key;
+        List<String> codes = (entry.value as List).cast<String>();
+        index[prefix] = codes;
+      }
+      return Success(index);
+    } on Exception catch(e) {
+      return Failure(e);
+    }
+  }
 }
