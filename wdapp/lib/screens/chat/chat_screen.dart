f4f6bc1626ee9ebbfdf9360644e2906f0ae0ac6c
Andy A <andy.stow.away@gmail.com>
2026-02-26 15:35:25 +0530

    added logout button to chat screen

--- wdapp/lib/screens/chat/chat_screen.dart
+++ wdapp/lib/screens/chat/chat_screen.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../widgets/logout_button.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    LogoutButtonModel viewModel = LogoutButtonModel(
+      authRepo: context.read(),
+    );
     return Scaffold(
       body: Center(
-        child: Text('Chat Screen!'),
+        child: LogoutButton(viewModel: viewModel),
       ),
     );
   }
