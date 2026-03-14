9483847102b0f581d1d34aa97b41ef89ee696145
Andy A <andy.stow.away@gmail.com>
2026-03-14 09:32:32 +0530

    added contact details model

---
+++ wdapp/lib/models/contact/contact_details.dart
@@ -0,0 +1,20 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'contact_details.freezed.dart';
+
+@freezed
+abstract class ContactDetails with _$ContactDetails {
+  
+  const ContactDetails._();
+
+  const factory ContactDetails({
+    required int age,
+    required String gender,
+    required String message,
+    required String loc,
+    required String location,
+    required String name,
+    required int timestamp,
+    required int unread,
+  }) = _ContactDetails;
+}
