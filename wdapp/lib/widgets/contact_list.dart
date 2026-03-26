886f3f7178e93f09b643db8d79ceb30058d5c695
Andy A <andy.stow.away@gmail.com>
2026-03-26 19:34:54 +0530

    added model to contact list and interaction via stream

--- wdapp/lib/widgets/contact_list.dart
+++ wdapp/lib/widgets/contact_list.dart
@@ -1,36 +1,93 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
+import '../models/events/events.dart';
+import '../models/contact/contact.dart';
 import '../models/contact/contact_details.dart';
 import '../widgets/contact_card.dart';
 
-class ContactList extends StatelessWidget {
-  const ContactList({
-    super.key,
-    required this.contacts,
-  });
-  
-  final List<ContactDetails> contacts;
+class ContactListModel {
+  ContactListModel() {
+    const mj = Contact(id:'+23USCA.MaryJane....');
+    contacts = <ContactDetails> [
+      ContactDetails(
+        name: mj.name,
+        age: mj.age,
+        loc: mj.loc,
+        gender: mj.gender,
+        unread: 0,
+        timestamp: 0,
+        location: 'California (United States of America)',
+        message: 'How is everything Mary?',
+      ),
+      ContactDetails(
+        name: mj.name,
+        age: mj.age,
+        loc: mj.loc,
+        gender: mj.gender,
+        unread: 3,
+        timestamp: 0,
+        location: 'New Jersy (United States of America)',
+        message: 'Mary me!!',
+      ),
+    ];
 
-  Widget _buildRegular(BuildContext context) {
+    _uecContactList.stream.listen(_handleUserEvents);
+  }
+
+  List<ContactDetails>? contacts;
+
+  final StreamController<List<ContactDetails>?> _scContactListModel = StreamController<List<ContactDetails>?>();
+  Stream<List<ContactDetails>?> get stateStream => _scContactListModel.stream;
+
+  final StreamController<UserEvent> _uecContactList = StreamController<UserEvent>();
+  Function(UserEvent) get sendUserEvent => _uecContactList.sink.add; 
+
+  Future<void> _handleUserEvents(UserEvent event) async {
+    switch(event) {
+      case UserEventSync:
+        _scContactListModel.sink.add(contacts);
+      default:
+        print("ContactListModel: no handler for event");
+    }
+  }
+}
+
+class ContactList extends StatelessWidget {
+  ContactList({
+    super.key,
+    required this.viewModel,
+  }): tapAction = viewModel.sendUserEvent;
+
+  final ContactListModel viewModel;
+  final Function(UserEvent) tapAction;
+
+  Widget _buildRegular(
+    BuildContext context,
+    List<ContactDetails>? contacts,
+  ) {
     return ListView.builder(
-      itemCount: contacts.length,
+      itemCount: contacts?.length,
       itemBuilder: (BuildContext context, int index) {
         return ContactCard(
-          contact: contacts[index],
-          onTapped: (){},
+          contact: contacts![index],
+          onTapped: () => tapAction(UserEventSync()),
         );
       },
     );
   }
 
-  Widget _buildCompact(BuildContext context) {
+  Widget _buildCompact(
+    BuildContext context,
+    List<ContactDetails>? contacts,
+  ) {
     return GridView.builder(
       padding: EdgeInsets.all(4.0),
-      itemCount: contacts.length,
+      itemCount: contacts?.length,
       itemBuilder: (BuildContext context, int index) {
         return ContactCard(
-          contact: contacts[index],
-          onTapped: (){},
+          contact: contacts![index],
+          onTapped: () => tapAction(UserEventSync()),
           isCompact: true,
         );
       },
@@ -40,15 +97,28 @@ class ContactList extends StatelessWidget {
       ),
     );
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (BuildContext context, BoxConstraints constraints) {
-        return constraints.maxHeight < 500 ?
-          _buildCompact(context):
-          _buildRegular(context);
+        bool isCompact = constraints.maxHeight < 500;
+        return StreamBuilder(
+          stream: viewModel.stateStream,
+          initialData: viewModel.contacts,
+          builder: (
+            BuildContext context, 
+            AsyncSnapshot<List<ContactDetails>?> snapshot
+          ){
+            if(snapshot.data == null || snapshot.data?.length == 0) {
+              return Center(child: Text("No Contacts!"));
+            }
+            return isCompact ? 
+              _buildCompact(context, snapshot.data):
+              _buildRegular(context, snapshot.data);
+          }
+        );  
       },
     );
-  }  
+  }
 }
