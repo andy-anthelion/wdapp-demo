8d431cdf2bcb25b479126c41e2713c14ab67c60a
Andy A <andy.stow.away@gmail.com>
2026-03-26 19:33:53 +0530

    header widget

---
+++ wdapp/lib/widgets/contact_header.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/logout_button.dart';
+
+class ContactHeader extends StatelessWidget {
+  const ContactHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    LogoutButtonModel viewModel = LogoutButtonModel(
+      authRepo: context.read()
+    );
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Text(
+              "WingDing",
+              style: TextTheme.of(context).headlineLarge!.copyWith(
+                color: ColorScheme.of(context).onSurfaceVariant,
+              ),
+            ),
+          ),
+          LogoutButton(viewModel: viewModel),
+        ],
+      ),
+    );
+  }
+}
